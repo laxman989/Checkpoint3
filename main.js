ae9779aafab854268fc15de12db58d69f1612b0f
laxman989 <laxmanchoudhary5685@gmail.com>
2023-12-02 19:37:33 +0530

    New changes

--- main.js
+++ main.js
@@ -344,3 +344,22 @@ document.getElementById('endDate').addEventListener('input', function () {
 document.getElementById('description').addEventListener('input', function () {
     descriptionError.textContent = "";
 });
+
+
+// Back to Top 
+document.addEventListener("DOMContentLoaded", function() {
+    var backToTopBtn = document.getElementById("back-to-top-btn");
+
+    window.addEventListener("scroll", function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopBtn.style.display = "block";
+        } else {
+            backToTopBtn.style.display = "none";
+        }
+    });
+
+    backToTopBtn.addEventListener("click", function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    });
+});
